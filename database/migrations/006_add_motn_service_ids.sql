c8b512b20805afda04b685972b582d4a0f4d63f7
L3kant <st.voboril@gmail.com>
2026-06-11 10:58:36 +0200

    Add Movie of the Night service IDs

---
+++ database/migrations/006_add_motn_service_ids.sql
@@ -0,0 +1,15 @@
+UPDATE streaming_services
+SET motn_service_id = 'netflix'
+WHERE service_name = 'Netflix';
+
+UPDATE streaming_services
+SET motn_service_id = 'hbo'
+WHERE service_name = 'Max';
+
+UPDATE streaming_services
+SET motn_service_id = 'disney'
+WHERE service_name = 'Disney+';
+
+UPDATE streaming_services
+SET motn_service_id = 'skyshowtime'
+WHERE service_name = 'SkyShowtime';
