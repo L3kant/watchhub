a0ea1667d7da85b9d4fe95f1a89623644b3329a9
L3kant <st.voboril@gmail.com>
2026-06-17 13:33:44 +0200

    Extract catalog detail services helper

--- routes/catalog.js
+++ routes/catalog.js
@@ -13,11 +13,8 @@ const {
   parseOptionalPositiveInteger,
 } = require('./catalog/queryParams');
 const { getServicesByTitleIds, getGenresByTitleIds } = require('./catalog/associations');
-const {
-  buildServiceLaunchLink,
-  getTmdbWatchUrl,
-  isSafeExternalLink,
-} = require('./catalog/launchers');
+const { getTmdbWatchUrl, isSafeExternalLink } = require('./catalog/launchers');
+const { getDetailServicesForTitle } = require('./catalog/detailServices');
 
 const router = express.Router();
 
@@ -51,47 +48,6 @@ function getPreferredStreamingOption(options, motnServiceId) {
   });
 
   return subscriptionOption || matchingOptions[0];
-}
-
-function getDetailServicesForTitle(title, titleId, blockedServices = []) {
-  const serviceParams = [titleId];
-  let blockedServicesSql = '';
-
-  if (blockedServices.length > 0) {
-    blockedServicesSql = `
-      AND ts.service_id NOT IN (${blockedServices.map(() => '?').join(', ')})
-    `;
-
-    serviceParams.push(...blockedServices);
-  }
-
-  const rawServices = db
-    .prepare(
-      `
-      SELECT
-        ss.service_id,
-        ss.service_name,
-        ss.motn_service_id,
-        ts.official_url,
-        ts.external_url,
-        ts.external_url_source,
-        ts.external_url_synced_at
-      FROM title_services ts
-      JOIN streaming_services ss
-        ON ss.service_id = ts.service_id
-      WHERE ts.title_id = ?
-        ${blockedServicesSql}
-      ORDER BY ss.service_name
-    `,
-    )
-    .all(...serviceParams);
-
-  return rawServices.map((service) => {
-    return {
-      ...service,
-      ...buildServiceLaunchLink(title, service),
-    };
-  });
 }
 
 function buildCatalogWhereClause(filters, profile) {
